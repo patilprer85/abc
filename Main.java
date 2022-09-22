5236b9a54196434f4dcfee518f80f083ae67764e
patilprer85 <PreranasanjaykumarP@hexaware.com>
2022-09-22 13:23:42 +0530

    Adding java file

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+    int c,n;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter number");
+    c=sc.nextInt();
+    n=c*c*c;
+    System.out.println("Cube="+n);
+        
+    }
+}
